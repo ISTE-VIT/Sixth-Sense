2be30f796e0397bfc91f2352677209a7391fc745
Hrithik Purwar <72293452+hrithikpurwar@users.noreply.github.com>
2021-11-20 14:06:49 +0530

    Update Image_Difference.py

--- Find Difference in Images/Image_Difference.py
+++ Find Difference in Images/Image_Difference.py
@@ -1,4 +1,4 @@
-from skimage.measure import compare_ssim
+from skimage.metrics import structural_similarity as ssim
 import imutils
 import cv2
 
@@ -8,16 +8,19 @@ gray1 = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
 
 #ssim has two parameter i.e score and diff
-(score,diff) = compare_ssim(gray1, gray2, full=True)
+(score,diff) = ssim(gray1, gray2, full=True)
 diff = (diff*255).astype("uint8")
 print("SSIM: {}".format(score))
 #The difference image is currently represented as a floating point data type in the range [0, 1] 
 #so we first convert the array to 8-bit unsigned integers in the range [0, 255] 
 #before we can further process it using OpenCV.
 
-thresh = cv2.threshold(diff,0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+#In thresholding, each pixel value is compared with the threshold value. 
+# If the pixel value is smaller than the threshold, it is set to 0, otherwise, 
+# it is set to a maximum value (generally 255).
+thresh = cv2.threshold(diff,0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1] #Thresholding
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #Finding contours
+cnts = imutils.grab_contours(cnts) #Grabbing contours
 
 #we threshold our diff  image using both cv2.THRESH_BINARY_INV  and cv2.THRESH_OTSU — 
 #both of these settings are applied at the same time using the vertical bar ‘or’ symbol, "|"
@@ -27,8 +30,8 @@ cnts = imutils.grab_contours(cnts)
 #now creating boundaries across the contours 
 for c in cnts:
     (x,y,w,h) =  cv2.boundingRect(c)
-    cv2.rectangle(image1,(x,y),(x+w,y+h), (0,0,255),2)
-    cv2.rectangle(image2,(x,y),(x+w,y+h), (0,0,255),2)  
+    cv2.rectangle(image1,(x,y),(x+w,y+h), (0,0,255),2) #Drawing rectangle BGR thickness
+    cv2.rectangle(image2,(x,y),(x+w,y+h), (0,0,255),2)  #Drawing rectangle BGR  thickness
     
 cv2.imshow("Image-1", image1)
 cv2.imshow("Image-2", image2)
